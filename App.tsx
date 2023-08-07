46647225656c52314c127b78ceb79bbad6f3cf34
nikhilmaske-2001 <nikhilmaske2001@gmail.com>
2023-08-07 10:00:51 +0530

    Added a switch to toggle between light and dark mode.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { myColors } from './src/styles/Colors';
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <View style={theme === 'light' ? styles.container : [styles.container, { backgroundColor: '#000' }]}>
+        <StatusBar style="auto" />
+        <Switch
+          style={{ transform: [{ scaleX: 1.4 }, { scaleY: 1.4 }] }}
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === "light" ? 'dark' : 'light')}
+        />
+      </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
     justifyContent: 'center',
   },
